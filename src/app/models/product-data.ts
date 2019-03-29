aae5e6c2b3227a2cd42fd523694e6e27dd1a54e8
Matt Groberg <grobergmg@gmail.com>
2019-03-29 09:36:06 -0700

    add product model

---
+++ src/app/models/product-data.ts
@@ -0,0 +1,47 @@
+export const products:Object[]=[
+  {
+    img:'',
+    description:'../assets/unicorn.jpg',
+    price: 5,
+    stars: 3,
+    badge: 'free-ship',
+    tags: ['earrings','jewlery','unicorn'],
+    discount:0
+  },
+  {
+    img:'',
+    description:'../assets/unicorn.jpg',
+    price: 2,
+    stars: 3,
+    badge: 'free-ship',
+    tags: ['earrings','jewlery','unicorn'],
+    discount:0
+  },
+  {
+    img:'',
+    description:'../assets/unicorn.jpg',
+    price: 4,
+    stars: 3,
+    badge: 'free-ship',
+    tags: ['earrings','jewlery','unicorn'],
+    discount:0
+  },
+  {
+    img:'',
+    description:'../assets/unicorn.jpg',
+    price: 2,
+    stars: 3,
+    badge: 'free-ship',
+    tags: ['earrings','jewlery','unicorn'],
+    discount:0
+  },
+  {
+    img:'',
+    description:'../assets/unicorn.jpg',
+    price: 17,
+    stars: 3,
+    badge: 'free-ship',
+    tags: ['earrings','jewlery','unicorn'],
+    discount:0
+  },
+]
