e01e1849bd1fd07afb3689058bd5db33aafa7254
Matt Groberg <grobergmg@gmail.com>
2019-03-29 11:06:17 -0700

    add conditional rendering of dropnav component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { categoryData } from '../models/category-data';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  selectedDrop:string;
+  selectedDrop:string = null;
   categories:string[]=categoryData;
   constructor() { }
 
