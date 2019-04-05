09034dc25412e4e35163248800c270765b01e73b
Matt Groberg <grobergmg@gmail.com>
2019-04-05 11:13:11 -0700

    connect review component to database

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../product.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers:[ProductService]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  reviews:FirebaseListObservable<any[]>;
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.reviews=this.productService.getReviews();
   }
 
 }
