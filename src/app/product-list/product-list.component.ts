0a6e6f047d02e1090946c87d5cb406003c6d2c5d
Matt Groberg <grobergmg@gmail.com>
2019-04-05 15:05:33 -0700

    fix rendering gap issues by switching to flexbox instead of grid on page comp

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,8 +4,7 @@ import { Router } from '@angular/router';
 import { ProductService } from '../product.service';
 import { Product } from '../models/product-model';
 import { FirebaseListObservable } from 'angularfire2/database';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-product-list',
@@ -15,21 +14,13 @@ import { Location } from '@angular/common';
 })
 export class ProductListComponent implements OnInit {
   products:FirebaseListObservable<any[]>;
-  selected:string='all';
-
-  constructor(private router: Router, private productService: ProductService, private route: ActivatedRoute,
-    private location: Location) { }
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit() {
     this.products=this.productService.getProducts();
-    this.route.params.forEach((urlParam)=>{
-      if(urlParam['category']){
-        this.selected=urlParam['category'];
-      }
-      console.log(this.selected);
-    })
   }
   goToDetailPage(clickedProduct) {
    this.router.navigate(['albums', clickedProduct.$key]);
  };
+
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product-model';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -8,6 +9,19 @@ import { Product } from '../models/product-model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent{
+export class ProductComponent implements OnInit {
   @Input() data: Product;
+  selected:string='all';
+  constructor(private route: ActivatedRoute,
+    private location: Location) { }
+
+  ngOnInit() {
+    this.route.params.forEach((urlParam)=>{
+      if(urlParam['category']){
+        this.selected=urlParam['category'];
+      }
+      console.log(this.selected);
+    })
+  }
+
 }
