14a912acf09c7ce00c60677ddc30de18de8bb685
Matt Groberg <grobergmg@gmail.com>
2019-04-05 09:18:18 -0700

    configure product row component and include on homepage

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProductService } from '../product.service';
+import { Product } from '../models/product-model';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-product-row',
   templateUrl: './product-row.component.html',
-  styleUrls: ['./product-row.component.scss']
+  styleUrls: ['./product-row.component.scss'],
+  providers: [ProductService]
 })
 export class ProductRowComponent implements OnInit {
-
-  constructor() { }
+  products:FirebaseListObservable<any[]>;
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit() {
+    this.products=this.productService.getProducts();
   }
 
 }
